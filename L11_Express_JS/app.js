bc9580e5db45d00b60699a18b64a96d37e01a5c5
Anjan012 <anjankhadka199@gmail.com>
2026-01-05 12:25:47 +0545

    create routes and learn params

--- L11_Express_JS/app.js
+++ L11_Express_JS/app.js
@@ -17,15 +17,53 @@ const app = express(); // app will get all the api's .. of express
 // app.delete
 
 // building home route
-app.get("/home", (req,res) => {
-    
-    const user = {
-        name: "Anjan",
-        age: 22, 
-        email: "anjan@example.com"
+// open web: http://localhost:8000/api/v1/home
+app.get("/api/v1/user/home", (req,res) => { // first arg: is string route and second arg: is callback function
+ 
+    res.send("<h1> Home Page </h1>")
+});
+
+// open web: http://localhost:8000/api/v1/about
+app.get("/api/v1/user/about", (req,res) => {
+ 
+    res.send("<h1> About Page </h1>")
+});
+
+
+// open web: http://localhost:8000/api/v1/profile
+app.get("/api/v1/user/profile", (req, res) => {
+    res.status(200).json({
+        success: true,
+        user: {
+            Username: "Anjan012",
+            email: "anjan@example.com"
+        }
+    })
+});
+
+// lets says i want a single product--------
+// in frontend we call it a dynamic route but in here we call it /api/ - api and productId a variable
+app.use("/api/v1/user/product/:productId/comment/:commentId", (req, res) => {
+
+    // req.params is an object 
+    // const productId = req.params.productId; // params is provided by express, here we are extracting the productId from url
+    const {productId, commentId} = req.params; // destructuring 
+    console.log(productId, commentId);
+
+    const product = {
+        id : "123",
+        name: "Macbook M1 pro"
     }
-    res.send(user)
-})
+
+    res.status(200).json({
+        success: true,
+        product
+    })
+    
+
+});
+
+// open web: http://localhost:8000/api/v1/user/product/5676/comment/321
 
 const PORT = 8000;
 
