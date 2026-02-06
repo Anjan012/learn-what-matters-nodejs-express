31350254440005118f63feb6e7e015434f44d3b4
Anjan012 <anjankhadka199@gmail.com>
2026-02-06 13:10:07 +0545

    image upload logic implementation code base seperation

--- Multer/app.js
+++ Multer/app.js
@@ -1,8 +1,6 @@
 import express from 'express';
-import crypto from "crypto";
-import path from "path";
-import multer from "multer";
 import cookieParser from "cookie-parser";
+import { upload } from './config/multerconfig.js';
 
 const app = express();
 
@@ -11,40 +9,6 @@ app.use(express.urlencoded({extended: true})); // urlencoded extended true used 
 app.use(cookieParser()); 
 app.use(express.json());
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './public/images/uploads')
-  },
-  filename: function (req, file, cb) {
-    // crypto is the built in nodejs module
-    crypto.randomBytes(12, (err, bytes) => {
-        console.log(bytes) // bytes gives us the buffer 
-        // path.extname takes out the extension from file
-        const fn = bytes.toString("hex") + path.extname(file.originalname);
-        cb(null, fn); // first is the error sencond is the fn is file name
-    })
-  }
-});
-
-const fileFilter = (req, file, cb) => {
-    const allowedMimeTypes = ['image/jpg', 'image/jpeg'];
-
-    if(allowedMimeTypes.includes(file.mimetype)){
-
-        cb(null, true);
-    }else {
-        cb(new Error("Only .jpg and .jpeg files are allowed!"), false);
-    }
-}
-
-const upload = multer({ 
-    storage: storage, 
-    fileFilter: fileFilter,
-    limits: {
-        fileSize: 5 * 1024 *1024,
-        files: 1
-    }
-});
 
 app.get("/", (req, res) => {
     res.status(200).json({
@@ -57,7 +21,7 @@ app.get("/test", (req, res) => {
 });
 
 app.post("/upload", upload.single("image"), (req, res) => {
-    res.render("test");
+    console.log(req.file);
 });
 
 app.listen(3000, () => {
